dfa7ad8d6ec86cc4884e6de10465d90572113d59
yeo-seoyun <yeosummer94@gmail.com>
2024-01-08 11:29:21 +0900

    공통컴포넌트 header, footer 마크업 [Feat] : 새로운 기능 추가 header, footer html,css 마크업
    -

---
+++ src/js/include.js
@@ -0,0 +1,17 @@
+fetch('/components/header.html')
+  .then((res) => res.text())
+  .then((data) => {
+    const header = document.querySelector('header');
+    header.innerHTML = data;
+    return import('/src/js/header.js');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+fetch('/components/footer.html')
+  .then((res) => res.text())
+  .then((data) => {
+    const footer = document.querySelector('footer');
+    footer.innerHTML = data;
+  });
