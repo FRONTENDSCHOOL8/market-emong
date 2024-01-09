ccc4faf1f5b93f6839eeb0b081fdd216cc5e587c
yeo-seoyun <yeosummer94@gmail.com>
2024-01-09 16:39:56 +0900

    장바구니 목록 토글 test까지 [feat] : 장바구니 목록 토글 test까지 장바구니 목록 토글 test까지
    -

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,1 +1,7 @@
-import PocketBase from 'pocketbase';
+// import PocketBase from 'pocketbase';
+
+document
+  .querySelector('.cart-togglebutton')
+  .addEventListener('click', function () {
+    document.querySelector('.cart-product').classList.toggle('hidden');
+  });
