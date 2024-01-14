548466327510d96608a606dc6d234ff943d9e8e9
yeo-seoyun <yeosummer94@gmail.com>
2024-01-14 20:20:40 +0900

    장바구니페이지 연결 [Feat] : header의 장바구니페이지 연결 장바구니페이지 연결 vite.config.js에 코드 추가
    -

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         header: resolve(__dirname, 'src/components/header.html'),
         footer: resolve(__dirname, 'src/components/footer.html'),
         login: resolve(__dirname, 'src/pages/login/index.html'),
+        cart: resolve(__dirname, 'src/pages/cart/index.html'),
         productList: resolve(__dirname, 'src/pages/productList/index.html'),
         signUp: resolve(__dirname, 'src/pages/signUp/index.html'),
       },
